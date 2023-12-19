47c8be1ad296bee79bbc6c099637f2185c1980db
prateekshuklaps0 <prateekshuklaps0@gmail.com>
2023-12-19 14:36:48 +0530

    checkout update

--- Frontend/README.md
+++ Frontend/README.md
@@ -1,3 +1,9 @@
+- Man Matters : https://manmatters.com/
+
+- Github Repo : https://github.com/anshul-010/Man-Matters
+
 - Lucide React NPM : https://www.npmjs.com/package/lucide-react/v/0.134.0
 
 - Lucide React : https://lucide.dev/
+
+- React Icons : https://react-icons.github.io/react-icons/
--- Frontend/src/Pages/Checkout.jsx
+++ Frontend/src/Pages/Checkout.jsx
@@ -1,14 +1,80 @@
 import * as css from "../Styles/CheckoutStyles";
 
-import React from "react";
-import { Box, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Text,
+  Image,
+  Step,
+  StepDescription,
+  StepIcon,
+  StepIndicator,
+  StepNumber,
+  StepSeparator,
+  StepStatus,
+  StepTitle,
+  Stepper,
+  useSteps,
+} from "@chakra-ui/react";
+import { useEffect } from "react";
+import { IoIosArrowBack as LeftArrow } from "react-icons/io";
 
 const Checkout = () => {
+  useEffect(() => {
+    const previousTitle = document.title;
+    document.title = "Checkout is Easy with Us - Man Matters";
+    return () => {
+      document.title = previousTitle;
+    };
+  }, []);
+
+  const steps = [
+    { title: "First", description: "Contact Info" },
+    { title: "Second", description: "Date & Time" },
+    { title: "Third", description: "Select Rooms" },
+  ];
+
+  const { activeStep } = useSteps({
+    index: 1,
+    count: steps.length,
+  });
+
   return (
     <Box>
-      <Box>
-        <Text>Checkout</Text>
+      {/* Navbar */}
+      <Box color="blackA" bg="whiteA" boxShadow="shadowB" css={css.NavOuter}>
+        <Box css={css.InnerNavCont}>
+          <Box css={css.LeftOptionsCont}>
+            <Image as={LeftArrow} />
+            <Text>Checkout</Text>
+          </Box>
+        </Box>
       </Box>
+
+      {/* Middle */}
+      <Box>
+        <Stepper index={activeStep}>
+          {steps.map((step, index) => (
+            <Step key={index}>
+              <StepIndicator>
+                <StepStatus
+                  complete={<StepIcon />}
+                  incomplete={<StepNumber />}
+                  active={<StepNumber />}
+                />
+              </StepIndicator>
+
+              <Box flexShrink="0">
+                <StepTitle>{step.title}</StepTitle>
+                <StepDescription>{step.description}</StepDescription>
+              </Box>
+
+              <StepSeparator />
+            </Step>
+          ))}
+        </Stepper>
+      </Box>
+
+      {/*  */}
     </Box>
   );
 };
