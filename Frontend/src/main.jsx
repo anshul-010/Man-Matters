b783d53e528ed4c4af312f01e3b7b66a7c4d9387
prateekshuklaps0 <prateekshuklaps0@gmail.com>
2023-12-19 11:33:35 +0530

    minor changes

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { ChakraProvider } from '@chakra-ui/react'
+import "./index.css";
+import App from "./App.jsx";
+import { store } from "./Redux/store.js";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from 'react-redux'
-import { store } from './Redux/store.js'
+import { Provider } from "react-redux";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <ChakraProvider>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ChakraProvider>
+    <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </ChakraProvider>
-  </BrowserRouter>
-)
+    </BrowserRouter>
+  </ChakraProvider>
+);
